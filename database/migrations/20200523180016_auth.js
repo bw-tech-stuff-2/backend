3df8cb512acafd6b6c9a0aa4ced309721b4baeda
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-23 19:11:46 -0400

    tech export.down added

--- database/migrations/20200523180016_auth.js
+++ database/migrations/20200523180016_auth.js
@@ -22,6 +22,7 @@ exports.up = function (knex) {
 
 exports.down = function (knex) {
     return knex.schema
+        .dropTableIfExists("tech")
         .dropTableIfExists("renters")
         .dropTableIfExists("owners")
 };
