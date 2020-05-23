9aa874a8724b3b2e957bf992c24ffbca6ecc0a8a
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-23 19:10:42 -0400

    tech table created

--- database/migrations/20200523180016_auth.js
+++ database/migrations/20200523180016_auth.js
@@ -11,7 +11,13 @@ exports.up = function (knex) {
       tbl.string("username", 50).index().notNullable().unique();
       tbl.string("password", 20).notNullable();
       tbl.string("renterName", 50).index().notNullable().unique();
-    });
+    })
+    .createTable("tech", tbl => {
+        tbl.increments();
+        tbl.string("techItem").notNullable();
+        tbl.string("seller").notNullable();
+        tbl.integer("price").notNullable();
+    })
 };
 
 exports.down = function (knex) {
