61bf1ac2104b2eb209e757e953dc6904af94556f
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-24 12:30:15 -0400

    tech seed updated

--- database/seeds/003_tech.js
+++ database/seeds/003_tech.js
@@ -1,13 +1,8 @@
 
+
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return knex('tech').insert([
+    {techItem: 'Time Traveling DeLorean', techDescription: 'This car can travel through time, please return yesterday.', owner: 'Doc', price: '$7.00'}, //1
+    {techItem: 'Mechanical Arm', techDescription: 'Need a hand?', owner: 'techgirl', price: '$1,000,000'}, //2
+  ]);
 };
