e646ff2b38e34f9bb600963917ddfa419d2291bc
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-24 18:51:56 -0400

    renters seeds updated

--- database/seeds/002_renters.js
+++ database/seeds/002_renters.js
@@ -1,7 +1,17 @@
 
 exports.seed = function(knex) {
-  return knex('renters').insert([
-    {username: 'flyguy', password: 'backtothefuture', renterName: 'Marty McFly', email: 'flyguy@mail.com'}, //1
-    {username: 'alchemist', password: 'fullmetal', renterName: 'Edward Elric', email: 'alchemist@mail,com'}, //2
-  ]);
+  return knex('renters').del()
+    .then(function () {
+      return knex('renters').insert([
+        {username: 'flyguy', password: hashPassword('backtothefuture'), renterName:'Marty McFly', email: 'flyguy@mail.com'},
+        {username: 'alchemist', password: hashPassword('fullmetal'), renterName:'Edward Elric', email: 'alchemist@mail.com'},
+      ]);
+    });
+};
+
+const hashPassword = password => {
+  const bcrypt = require('bcryptjs');
+  const rounds = 12;
+  const hash = bcrypt.hashSync(password, rounds);
+  return hash;
 };
