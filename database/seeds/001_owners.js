16ac28c5686de3e1a78acd3335ded86dbaa661ef
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-24 18:41:06 -0400

    owners seed updated

--- database/seeds/001_owners.js
+++ database/seeds/001_owners.js
@@ -1,7 +1,17 @@
 
 exports.seed = function(knex) {
-  return knex('owners').insert([
-    {username: 'Doc', password: 'backtothefuture', ownerName: 'Emmett Brown', email: 'docbrown@mail.com'}, //1
-    {username: 'techgirl', password: 'fullmetal', ownerName: 'Winry Rockbell', email: 'techgirl@mail,com'}, //2
-  ]);
+  return knex('owners').del()
+    .then(function () {
+      return knex('owners').insert([
+        {username: 'Doc', password: hashPassword('backtothefuture'), ownerName:'Emmett Brown', email: 'docbrown@mail.com'},
+        {username: 'techgirl', password: hashPassword('fullmetal'), ownerName:'Winry Rockbell', email: 'techgirl@mail.com'},
+      ]);
+    });
+};
+
+const hashPassword = password => {
+  const bcrypt = require('bcryptjs');
+  const rounds = 12;
+  const hash = bcrypt.hashSync(password, rounds);
+  return hash;
 };
