e671cd59b4ed0c6e848f361c7332f0cf15d3a05b
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-23 22:01:04 -0400

    users-model.js updated

--- users/users-router.js
+++ users/users-router.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 
 const Users = require("./users-model.js");
-const restricted = require("../auth/restricted-middleware.js");
+const restricted = require("../auth/authenticate-middleware.js");
 const { isValid } = require("./users-service.js");
 
 router.use(restricted);
