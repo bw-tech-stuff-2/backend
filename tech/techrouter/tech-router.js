da2e1e6677d6f977f19c653b77b274a6835a8b94
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-24 14:12:01 -0400

    get by id setup tech-router

--- tech/techrouter/tech-router.js
+++ tech/techrouter/tech-router.js
@@ -12,4 +12,20 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Tech.findById(id)
+    .then(tech => {
+        if (tech) {
+            res.status(200).json(tech)
+        } else {
+            res.status(404).json({message: 'Could not find that tech'})
+        }
+    })
+    .catch(error => {
+        res.status(500).json({message: error})
+    })
+})
+
 module.exports = router;
