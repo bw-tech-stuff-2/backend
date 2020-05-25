f5709af6a61ca8a0301b6956d78ae5f5c3bf4843
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-25 00:23:11 -0400

    find setup in request-model

--- tech/requestrouter/request-model.js
+++ tech/requestrouter/request-model.js
@@ -8,3 +8,10 @@ module.exports = {
     update,
     remove
 }
+
+function find() {
+    return db('techRequest')
+        .join('tech', 'techRequest.techId', 'tech.id')
+        .join('renters', 'techRequest.rentersId', 'renters.id')
+        .select('techRequest.id', 'tech.techItem', 'tech.techDescription', 'tech.owner', 'tech.price','techRequest.request', 'techRequest.rentersId', 'renters.name')
+}
