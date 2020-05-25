4f220f39e182b64fc62fb20820d6dbb3d1497b51
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-25 15:22:45 -0400

    config updated

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -1,8 +1,9 @@
 const knex = require("knex");
 
-const knexconfig = require("../knexfile.js");
-const environment = process.env.NODE_ENV || "development";
+const knexConfig = require("../knexfile.js");
+// const environment = process.env.NODE_ENV || "development";
 
-module.exports = knex(knexconfig[environment]);
+// module.exports = knex(knexconfig[environment]);
+module.exports = knex(knexConfig.development);
 
 // ('development') this will break postman, but needed for testing
