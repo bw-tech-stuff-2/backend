035614a94115b509d685eb4e1d72f8040c4e9af0
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-25 01:18:06 -0400

    findById get setup in request-router

--- tech/requestrouter/request-router.js
+++ tech/requestrouter/request-router.js
@@ -12,4 +12,20 @@ router.get('/', (req, res) => {
     })
 })
 
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+
+    Request.findById(id)
+    .then(requests => {
+        if (requests) {
+            res.status(200).json(requests)
+        } else {
+            res.status(404).json({message: 'Could not find id'})
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
+
 module.exports = router;
