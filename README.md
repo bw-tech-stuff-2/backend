4ff3b18412f632434727ad9136f431c97d3d9bf5
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-26 16:30:50 -0400

    readme updated, tests added

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
 BE Endpoints
 
+
+
+# Owners
+| Name | Type | Required | Unique | Notes |
+| ---- | ---- | -------- | ------ | ----- |
+| username | string | yes | yes | owners username |
+| password | string | yes | no | owners password |
+| ownerName | string | yes | no | owners name |
+| email | string | no | no | owners email) |
+
 Register New Owner POST to /api/owners/auth/register
 
  {
@@ -35,6 +45,123 @@ Login New Renter POST to /api/renters/auth/login
     password:    backtothefuture   // String, NEEDED
     renterName:  Marty McFly       // String, NEEDED
  }
+
+
+
+Owner goes to Tech and returns array of tech GET to /api/tech/
+
+[
+    {
+        "id": 1,
+        "techItem": "Time Traveling DeLorean",
+        "techDescription": "This car can travel through time, please return yesterday.",
+        "owner": "Doc",
+        "price": "$7.00"
+    },
+    {
+        "id": 2,
+        "techItem": "Mechanical Arm",
+        "techDescription": "Need a hand?",
+        "owner": "techgirl",
+        "price": "$1,000,000"
+    }
+]
+
+Owner gets tech by id GET to /api/tech/:id
+Example /api/tech/1
+
+    {
+        "id": 1,      // this is the tech id
+        "techItem": "Time Traveling DeLorean",
+        "techDescription": "This car can travel through time, please return yesterday.",
+        "owner": "Doc",
+        "price": "$7.00"
+    }
+
+   Owner adds item to tech list POST to api/tech/
+
+    {
+        "techItem": "Time Traveling DeLorean",
+        "techDescription": "This car can travel through time, please return yesterday.",
+        "owner": "Doc",
+        "price": "$70.00",
+        "ownersId": "2"
+    }
+
+    Owner edits item on tech list PUT to api/tech/:id
+    example api/tech/1
+
+    {
+        "techItem": "Time Traveling DeLorean",
+        "techDescription": "This car can travel through time, please return yesterday.",
+        "owner": "Doc",
+        "price": "$700.00"
+    }
+
+    Owner deletes item on tech list DELETE to api/tech/:id
+    example api/tech/1
+
+
+    Renter goes to request and returns an array of tech GET to /api/request
+
+    [
+    {
+        "id": 1,
+        "techItem": "Time Traveling DeLorean",
+        "techDescription": "This car can travel through time, please return yesterday.",
+        "owner": "Doc",
+        "price": "$7.00",
+        "request": "I need this now!!",
+        "rentersId": 1,
+        "username": "flyguy"
+    },
+    {
+        "id": 2,
+        "techItem": "Mechanical Arm",
+        "techDescription": "Need a hand?",
+        "owner": "techgirl",
+        "price": "$1,000,000",
+        "request": "I need this by Friday",
+        "rentersId": 2,
+        "username": "alchemist"
+    }
+    ]
+
+    Renter gets tech by id GET to /api/request/:id
+    example /api/request/2 
+
+    {
+    "id": 2,
+    "techItem": "Mechanical Arm",
+    "techDescription": "Need a hand?",
+    "owner": "techgirl",
+    "price": "$1,000,000",
+    "request": "I need this by Friday",
+    "rentersId": 2,
+    "username": "alchemist"
+    }
+
+    Renter adds tech by POST to /api/request/
+    example /api/request/
+
+    {
+        "techId": "1",         //the id of the tech not the id of the request
+        "request": "yessss pleasessssss",
+        "rentersId": "2"
+    }
+
+    Renter edits by id PUT to /api/request/:id
+    example /api/request/2
+
+    {
+    "request": "I need this now!!",
+    "rentersId": 2
+    }
+
+    Renter deletes request by id DELETE to /api/request/:id
+    example /api/request/2
+
+    
 
 
 
--- api/server.spec.js
+++ api/server.spec.js
@@ -28,3 +28,117 @@ describe("GET /", () => {
           });
       });
     });
+
+    test("POST /api/owners/register to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/owners/auth/register")
+        .send({ username: "", password: "meow" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/owners/register to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/owners/auth/register")
+        .send({ username: "meow", password: "" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/owners/register to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/owners/auth/register")
+        .send({ username: "", password: "" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/renters/register to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/renters/auth/register")
+        .send({ username: "", password: "meow" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/renters/register to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/renters/auth/register")
+        .send({ username: "meow", password: "" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/renters/register to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/renters/auth/register")
+        .send({ username: "", password: "" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/owners/auth/login to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/owners/auth/login")
+        .send({ username: "kitty", password: "meow" });
+      expect(res.status).toBe(401);
+    });
+
+    test("POST /api/owners/auth/login to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/owners/auth/login")
+        .send({ username: "", password: "meow" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/owners/auth/login to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/owners/auth/login")
+        .send({ username: "", password: "" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/renters/auth/login to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/renters/auth/login")
+        .send({ username: "kitty", password: "meow" });
+      expect(res.status).toBe(401);
+    });
+
+    test("POST /api/renters/auth/login to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/renters/auth/login")
+        .send({ username: "", password: "meow" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
+
+    test("POST /api/renters/auth/login to be failing", async () => {
+      const res = await supertest(server)
+        .post("/api/renters/auth/login")
+        .send({ username: "", password: "" });
+      expect(res.status).toBe(400);
+      expect(res.body).toMatchObject({
+        message: "please provide username and password and the password should be alphanumeric"
+      });
+    });
