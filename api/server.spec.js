bc0f82b42fe5b21b9d183d1014c7535c9ce4bbdc
Kenneth Morlock <kennethmorlock@Kenneths-MBP.fios-router.home>
2020-05-27 11:58:46 -0400

    get tests added for tech

--- api/server.spec.js
+++ api/server.spec.js
@@ -3,6 +3,9 @@ const supertest = require("supertest");
 const server = require("./server.js");
 const db = require("../database/dbConfig");
 
+
+//tests for /
+
 describe("GET /", () => {
     it("should return http status code 200 OK", () => {
       return (
@@ -29,6 +32,10 @@ describe("GET /", () => {
       });
     });
 
+
+
+    //register and login tests
+
     test("POST /api/owners/register to be failing", async () => {
       const res = await supertest(server)
         .post("/api/owners/auth/register")
@@ -142,3 +149,81 @@ describe("GET /", () => {
         message: "please provide username and password and the password should be alphanumeric"
       });
     });
+
+    //tech tests
+
+
+    describe("GET /tech", () => {
+      it("should return http status code 200 OK", () => {
+        return (
+          supertest(server)
+            .get("/api/tech")
+            .then(response => {
+              // from jest
+              expect(response.status).toBe(400);
+            })
+        );
+      });
+    })
+
+    describe("GET /tech", () => {
+    it("should return { api: 'Please provide the authentication information' }", () => {
+      return supertest(server)
+        .get("/api/tech")
+        .then(response => {
+          expect(response.body).toMatchObject({
+            message: "Please provide the authentication information"
+          });
+        });
+    });
+  });
+
+  describe("GET /tech/:id", () => {
+    it("should return http status code 200 OK", () => {
+      return (
+        supertest(server)
+          .get("/api/tech/2")
+          .then(response => {
+            // from jest
+            expect(response.status).toBe(400);
+          })
+      );
+    });
+  })
+
+  describe("GET /tech/:id", () => {
+  it("should return { api: 'Please provide the authentication information' }", () => {
+    return supertest(server)
+      .get("/api/tech/2")
+      .then(response => {
+        expect(response.body).toMatchObject({
+          message: "Please provide the authentication information"
+        });
+      });
+  });
+});
+
+describe("GET /tech/:id", () => {
+  it("should return http status code 200 OK", () => {
+    return (
+      supertest(server)
+        .get("/api/tech/2")
+        .then(response => {
+          // from jest
+          expect(response.status).toBe(400);
+        })
+    );
+  });
+})
+
+describe("GET /tech/owners/:id", () => {
+it("should return { api: 'Please provide the authentication information' }", () => {
+  return supertest(server)
+    .get("/api/tech/owners/2")
+    .then(response => {
+      expect(response.body).toMatchObject({
+        message: "Please provide the authentication information"
+      });
+    });
+});
+});
